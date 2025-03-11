758c4f32157c0eef275cbcf39b045f2502f00f3e
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-11 19:26:16 +0900

    #1: 타입 설정

--- src/types/Items.ts
+++ src/types/Items.ts
@@ -1,14 +1,16 @@
-type TotalGold = {
-  total: number;
-};
-
-type Image = {
-  group: string;
-};
-
 export type Item = {
   name: string;
-  description: string;
-  image: Image;
-  gold: TotalGold;
+  plaintext: string;
+  image: {
+    full: string;
+  };
+  gold: {
+    total: number;
+  };
+};
+
+export type ItemData = {
+  data: {
+    [id: string]: Item;
+  };
 };
