9ba130f35c438736ccf467977a2e765c4dc774cd
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-11 19:26:28 +0900

    #1: 페이지 로직 작성

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,5 +1,20 @@
-const ItemsPage = () => {
-  return <div>ItemsPage</div>;
+import ItemList from "@/components/ItemList";
+import { fetchItemsData } from "@/utils/serverApi";
+
+const ItemsPage = async () => {
+  const { ItemsData } = await fetchItemsData();
+  const arrItemsData = Object.values(ItemsData.data);
+
+  return (
+    <div>
+      <h1>ItemsPage</h1>
+      <ul>
+        {arrItemsData.map((item) => {
+          return <ItemList key={item.name} item={item} />;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default ItemsPage;
