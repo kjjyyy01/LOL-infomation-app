5ab590d87ced64992ffcd5d5b7a0a1aedba44c42
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-12 16:52:42 +0900

    #3: 챔피언 데이터 fetching 로직 작성

--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -1,12 +1,13 @@
 "use server";
 
 import { VERSIONS_API_URL } from "@/constants/url";
-import { ItemData } from "@/types/Items";
+import { ChampionType } from "@/types/Champion";
+import { ItemType } from "@/types/Items";
 
 export const fetchVersionData = async (): Promise<string> => {
-  const versionResponse = await fetch(VERSIONS_API_URL,{
-    method:"GET",
-    cache:"force-cache",
+  const versionResponse = await fetch(VERSIONS_API_URL, {
+    method: "GET",
+    cache: "force-cache",
   });
   const versionData: string[] = await versionResponse.json();
   const currentVersion = versionData[0];
@@ -14,14 +15,32 @@ export const fetchVersionData = async (): Promise<string> => {
   return currentVersion;
 };
 
-export const fetchItemsData = async (): Promise<{ ItemsData: ItemData }> => {
+export const fetchItemsData = async (): Promise<{ ItemsData: ItemType }> => {
   const currentVersion = await fetchVersionData();
 
   const ItemsResponse = await fetch(`https://ddragon.leagueoflegends.com/cdn/${currentVersion}/data/ko_KR/item.json`, {
     method: "GET",
     cache: "force-cache",
   });
-  const ItemsData: ItemData = await ItemsResponse.json();
+  const ItemsData: ItemType = await ItemsResponse.json();
 
   return { ItemsData };
+};
+
+export const fetchChampionsData = async (): Promise<{ championsData: ChampionType }> => {
+  const currentVersion = await fetchVersionData();
+
+  const championsResponse = await fetch(
+    `https://ddragon.leagueoflegends.com/cdn/${currentVersion}/data/ko_KR/champion.json`,
+    {
+      method: "GET",
+      next: {
+        revalidate: 86400,
+      },
+    }
+  );
+  const championsData: ChampionType = await championsResponse.json();
+  console.log(championsData);
+
+  return { championsData };
 };
