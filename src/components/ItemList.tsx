542fa8db783db74edc38a44afc774d16cac7130e
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-12 15:19:26 +0900

    #3: 아이템 페이지 코드 일부 수정

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 import React from "react";
 
 const ItemList = ({ item }: { item: Item }) => {
-  const imageFileName = item.image.full;
-  const itemImageURL = `https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/${imageFileName}`;
+  const imageFileName: string = item.image.full;
+  const itemImageURL: string = `https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/${imageFileName}`;
   return (
     <li>
-      <Image src={itemImageURL} alt={item.name} width={100} height={100} priority />
+      <Image src={itemImageURL} alt={item.name} width={100} height={100} />
       <h3>{item.name}</h3>
       <p>{item.plaintext}</p>
     </li>
--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -4,17 +4,21 @@ import { VERSIONS_API_URL } from "@/constants/url";
 import { ItemData } from "@/types/Items";
 
 export const fetchVersionData = async (): Promise<string> => {
-  const versionResponse = await fetch(VERSIONS_API_URL);
+  const versionResponse = await fetch(VERSIONS_API_URL,{
+    method:"GET",
+    cache:"force-cache",
+  });
   const versionData: string[] = await versionResponse.json();
   const currentVersion = versionData[0];
 
   return currentVersion;
 };
 
-export const fetchItemsData = async () => {
+export const fetchItemsData = async (): Promise<{ ItemsData: ItemData }> => {
   const currentVersion = await fetchVersionData();
 
   const ItemsResponse = await fetch(`https://ddragon.leagueoflegends.com/cdn/${currentVersion}/data/ko_KR/item.json`, {
+    method: "GET",
     cache: "force-cache",
   });
   const ItemsData: ItemData = await ItemsResponse.json();
