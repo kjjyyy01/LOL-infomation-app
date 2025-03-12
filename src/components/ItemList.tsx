b13fc5a25121bc11c12581bc3c187e9f7f729646
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-12 16:51:20 +0900

    #3: 변수명 변경

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -3,13 +3,14 @@ import Image from "next/image";
 import React from "react";
 
 const ItemList = ({ item }: { item: Item }) => {
-  const imageFileName: string = item.image.full;
-  const itemImageURL: string = `https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/${imageFileName}`;
+  const itemsImageFileName: string = item.image.full;
+  const itemImageURL: string = `https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/${itemsImageFileName}`;
   return (
     <li>
       <Image src={itemImageURL} alt={item.name} width={100} height={100} />
       <h3>{item.name}</h3>
       <p>{item.plaintext}</p>
+      <p>{item.gold.total} gold</p>
     </li>
   );
 };
