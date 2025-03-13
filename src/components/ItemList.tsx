da14cdd7a1d74caa3637576e0db091b724b9db0e
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-13 20:50:48 +0900

    #4: 버전 정보 fetch로직 추가

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,10 +1,13 @@
+import { BASE_URL } from "@/constants/url";
 import { Item } from "@/types/Items";
+import { fetchVersionData } from "@/utils/serverApi";
 import Image from "next/image";
 import React from "react";
 
-const ItemList = ({ item }: { item: Item }) => {
+const ItemList = async ({ item }: { item: Item }) => {
+  const currentVersion = await fetchVersionData();
   const itemsImageFileName: string = item.image.full;
-  const itemImageURL: string = `https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/${itemsImageFileName}`;
+  const itemImageURL: string = `${BASE_URL}/cdn/${currentVersion}/img/item/${itemsImageFileName}`;
   return (
     <li>
       <Image src={itemImageURL} alt={item.name} width={100} height={100} />
