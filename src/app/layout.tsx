2769e41fdb3a3b39d0c856d0f373f7c015dca491
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-14 10:56:49 +0900

    #9: 상표권 및 공식 인증 여부 관련 푸터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,11 @@ export default function RootLayout({
         </nav>
         {children}
       </body>
+      <footer className="sticky bottom-10">
+        LOLI is not endorsed by Riot Games and does not reflect the views or opinions of Riot Games or anyone officially
+        involved in producing or managing Riot Games properties. Riot Games and all associated properties are trademarks
+        or registered trademarks of Riot Games, Inc.
+      </footer>
     </html>
   );
 }
