02e74e7932481cadc5174664b1a6d5a1359dd21f
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-14 11:02:17 +0900

    #9: 홈페이지 구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "LOLI",
+  description: "League of Legend Information Homepage",
+};
+
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <p>리그오브레전드 챔피언 목록 및 아이템 목록 정보를 제공합니다.</p>
+      <h1>LOLI</h1>
+      <p>
+        League of Legend Information에 오신것을 환영합니다.
+        <br /> 최신 리그오브레전드 챔피언과 아이템 정보를 한눈에 볼 수 있습니다.
+        <br /> 필요한 정보가 있다면, 찾아보세요!!
+      </p>
+      <Link href={"/champions"}>
+        <Image src={"/image/homeImage1.png"} alt="챔피언 페이지 링크 이미지" width={500} height={100} />
+        챔피언
+      </Link>
+      <br />
+      <Link href={"/items"}>
+        <Image src={"/image/homeImage2.png"} alt="아이템 페이지 링크 이미지" width={500} height={100} />
+        아이템
+      </Link>
+      <br />
+      <Link href={"/rotation"}>
+        <Image src={"/image/homeImage3.png"} alt="로테이션 페이지 링크 이미지" width={500} height={100} />
+        로테이션
+      </Link>
     </div>
   );
 }
