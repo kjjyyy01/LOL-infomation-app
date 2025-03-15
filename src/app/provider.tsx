7be95d14e2c3b7d41cdde19ffd29ae46ff9a94a7
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-15 21:23:23 +0900

    #5: tanstackQuery를 사용하기 위한 provider생성

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,9 +1,30 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
+"use client";
 
-const Provider = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = new QueryClient();
+import { isServer, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
-};
-
-export default Provider;
+}
