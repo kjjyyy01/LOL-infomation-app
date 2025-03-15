3e8c5f33755cdb37607ed5b89dce1bfaae79149e
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-15 21:22:48 +0900

    #5: url 상수화

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,2 +1,4 @@
 export const VERSIONS_API_URL = "https://ddragon.leagueoflegends.com/api/versions.json";
 export const BASE_URL = "https://ddragon.leagueoflegends.com";
+export const ROTATION_API_URL = "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations";
+export const RIOT_DEVELOPER_ORIGIN_URL = "https://developer.riotgames.com";
