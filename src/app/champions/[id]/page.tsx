07010eab579bc9f214c39b03a0cd6e12ff504010
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-16 22:06:21 +0900

    #6: 상세페이지 스타일링

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -1,6 +1,8 @@
+import { FLEX_COL_CENTER, WRAPPER_STYLE } from "@/constants/style";
 import { BASE_URL } from "@/constants/url";
 import { ChampionDetail, ChampionsDetailPageProps, Passive, Spells } from "@/types/Champion";
 import { fetchChampionsDetailData, fetchVersionData } from "@/utils/serverApi";
+import clsx from "clsx";
 import { Metadata } from "next";
 import Image from "next/image";
 
@@ -19,47 +21,48 @@ const ChampionsDetailPage = async ({ params }: ChampionsDetailPageProps) => {
   const passive: Passive = arrChampionsDetailData[0].passive;
 
   return (
-    <div>
+    <div className={clsx(FLEX_COL_CENTER, "gap-6", " my-6")}>
       <h1>챔피언 상세</h1>
       <section>
         {arrChampionsDetailData.map((champion) => {
           return (
-            <div key={champion.id}>
-              <Image src={itemImageURL} alt={champion.name} width={100} height={1000} className="w-full" />
+            <div key={champion.id} className={clsx(FLEX_COL_CENTER, "gap-4")}>
+              <Image src={itemImageURL} alt={champion.name} width={100} height={100} className="w-[700px]" />
               <h3>{champion.name}</h3>
               <p>{champion.title}</p>
-              <p>{champion.lore}</p>
+              <p className="text-center">{champion.lore}</p>
             </div>
           );
         })}
       </section>
-      <section>
-        <h1>패시브</h1>
-        <Image
-          src={`${BASE_URL}/cdn/${currentVersion}/img/passive/${passive.image.full}`}
-          alt={passive.image.full}
-          width={100}
-          height={100}
-        />
-        <h4>{passive.name}</h4>
-        <p>{passive.description}</p>
-        <h1>스킬(Q,W,E,R 순서)</h1>
-        <ul>
+      <section className={clsx(FLEX_COL_CENTER, "gap-6")}>
+        <h2>스킬(패시브, Q, W, E, R 순서)</h2>
+        <section className="flex flex-row justify-center items-center">
+          <div className={WRAPPER_STYLE}>
+            <Image
+              src={`${BASE_URL}/cdn/${currentVersion}/img/passive/${passive.image.full}`}
+              alt={passive.image.full}
+              width={100}
+              height={100}
+            />
+            <h3>{passive.name}</h3>
+            <p className="text-start">{passive.description}</p>
+          </div>
           {spells.map((spell) => {
             return (
-              <li key={spell.id}>
+              <div key={spell.id} className={WRAPPER_STYLE}>
                 <Image
                   src={`${BASE_URL}/cdn/${currentVersion}/img/spell/${spell.image.full}`}
                   alt={spell.name}
                   width={100}
                   height={100}
                 />
-                <h4>{spell.name}</h4>
-                <p>{spell.description}</p>
-              </li>
+                <h3>{spell.name}</h3>
+                <p className="text-start">{spell.description}</p>
+              </div>
             );
           })}
-        </ul>
+        </section>
       </section>
     </div>
   );
