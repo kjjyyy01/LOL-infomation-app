4da7b9f93753e9265df6bcea1da387ad1609ee02
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-16 13:25:08 +0900

    #5: 로테이션 챔피언 fetching하는 로직 작성

--- src/utils/clientApi.ts
+++ src/utils/clientApi.ts
@@ -1,8 +1,8 @@
 "use client";
 
-import { ChampionRotation } from "@/types/ChampionRotation";
+import { Champion } from "@/types/Champion";
 
-export const fetchRotationData = async (): Promise<ChampionRotation> => {
+export const fetchRotationData = async (): Promise<Champion[]> => {
   const res = await fetch("api/rotation/");
   return await res.json();
 };
