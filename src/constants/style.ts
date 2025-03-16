ff3afc1146791bdc0e4fbd1634a2cc12f578009c
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-16 22:06:35 +0900

    #6: 중복 스타일 상수화

---
+++ src/constants/style.ts
@@ -0,0 +1,2 @@
+export const WRAPPER_STYLE = "flex flex-col justify-center items-center gap-1 border-2 border-black p-2 mx-2";
+export const FLEX_COL_CENTER = "flex flex-col justify-center items-center";
