6d7ac85408e61cd9f23eff6d2185f234bf1de9e4
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-16 23:05:56 +0900

    #6: 배포중 오류를 위한 수정

--- src/constants/style.ts
+++ src/constants/style.ts
@@ -1,4 +1,4 @@
 export const SPELL_WRAPPER_STYLE = "flex flex-col justify-center items-center gap-1 border-2 border-black p-2 mx-2";
 export const FLEX_COL_CENTER = "flex flex-col justify-center items-center";
-export const TEXT_CENTER = "text-center"
-export const HOVER_LINK = "hover:underline"
+export const TEXT_CENTER = "text-center";
+export const HOVER_LINK = "hover:underline";
