ff5ddbd1f1bf4d59629459fe316b0dde026848da
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-16 22:28:56 +0900

    #6: Link를 통한 navigate하는 경로 상수화

---
+++ src/constants/navigate.ts
@@ -0,0 +1,18 @@
+export const NAV_MENUS = {
+  HOME: {
+    to: "/",
+    label: "Home",
+  },
+  CHAMPIONS: {
+    to: "/champions",
+    label: "Champions",
+  },
+  ROTATION: {
+    to: "/rotation",
+    label: "Rotation",
+  },
+  ITEMS: {
+    to: "/items",
+    label: "Items",
+  },
+};
