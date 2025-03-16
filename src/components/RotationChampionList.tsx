3dcbd4e50b69c7459e8443b24f1c86c021a51789
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-16 13:25:57 +0900

    #5: 로테이션리스트 분리

---
+++ src/components/RotationChampionList.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { BASE_URL } from "@/constants/url";
+import { Champion } from "@/types/Champion";
+import Image from "next/image";
+import Link from "next/link";
+
+const RotationChampionList = ({ champion, currentVersion }: { champion: Champion; currentVersion: string }) => {
+  const championsImageFileName: string = champion.image.full;
+  const championImageURL: string = `${BASE_URL}/cdn/${currentVersion}/img/champion/${championsImageFileName}`;
+
+  return (
+    <Link href={`/rotation/${champion.id}`}>
+      <li key={champion.name}>
+        <Image src={championImageURL} alt={champion.name} width={100} height={100} />
+        <h3>{champion.name}</h3>
+        <p>{champion.title}</p>
+      </li>
+    </Link>
+  );
+};
+
+export default RotationChampionList;
