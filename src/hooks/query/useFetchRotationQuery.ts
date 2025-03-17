41b2c35b72e991a97fb3c166fbc1f48539cc3cfa
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-17 16:36:03 +0900

    #10: 파일 구조 리팩토링

--- src/hooks/query/useFetchRotationQuery.ts
+++ src/hooks/query/useFetchRotationQuery.ts
@@ -1,5 +1,5 @@
 import { Champion } from "@/types/Champion";
-import { fetchRotationData } from "@/utils/clientApi";
+import { fetchRotationData } from "@/utils/routeHandler/clientApi";
 import { useQuery } from "@tanstack/react-query";
 
 export function useFetchRotationFunction() {
