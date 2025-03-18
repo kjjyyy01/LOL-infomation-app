4143bb3c27a7fc3830c65bb2f5267538334e13d3
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-18 16:55:01 +0900

    #13: 반응형 디자인 수정

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -17,7 +17,7 @@ const ChampionsPage = async () => {
   const arrChampionsData: Champion[] = Object.values(championsData.data);
 
   return (
-    <div className={clsx(FLEX_COL_CENTER, "gap-6", "my-6", "p-2", "")}>
+    <div className={clsx(FLEX_COL_CENTER, "gap-6", "my-6", "p-2")}>
       <h1>챔피언</h1>
       <ul>
         {arrChampionsData.map((champion) => {
