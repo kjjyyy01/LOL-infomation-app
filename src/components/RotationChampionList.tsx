1d1b5d06a47a9f1b6540213f2f37f9d1a2ef6cfb
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-18 12:48:27 +0900

    #13: 유틸리티타입 사용

--- src/components/RotationChampionList.tsx
+++ src/components/RotationChampionList.tsx
@@ -7,7 +7,13 @@ import { Champion } from "@/types/Champion";
 import Image from "next/image";
 import Link from "next/link";
 
-const RotationChampionList = ({ champion, currentVersion }: { champion: Champion; currentVersion: string }) => {
+const RotationChampionList = ({
+  champion,
+  currentVersion,
+}: {
+  champion: Omit<Champion, "key">;
+  currentVersion: string;
+}) => {
   const championsImageFileName: string = champion.image.full;
   const championImageURL: string = `${BASE_URL}/cdn/${currentVersion}/img/champion/${championsImageFileName}`;
 
