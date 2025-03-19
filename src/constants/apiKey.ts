0f6481d820fca542af9bc20a410c614e916c7e9d
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-19 17:27:33 +0900

    코드 수정

--- src/constants/apiKey.ts
+++ src/constants/apiKey.ts
@@ -1,2 +1,2 @@
 export const API_KEY = process.env.RIOT_API_KEY;
-export const NEXT_PUBLIC_API_KEY = process.env.NEXT_PUBLIC_RIOT_API_KEY;
+export const NEXT_PUBLIC_HOST = process.env.NEXT_PUBLIC_HOST;
