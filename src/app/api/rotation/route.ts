aac8ac3ecdac86808a55130e39dce5d0bd544c2a
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-19 16:22:31 +0900

    코드 수정

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -1,4 +1,4 @@
-import { NEXT_PUBLIC_API_KEY } from "@/constants/apiKey";
+import { API_KEY } from "@/constants/apiKey";
 import { RIOT_DEVELOPER_ORIGIN_URL, ROTATION_API_URL } from "@/constants/url";
 import { Champion } from "@/types/Champion";
 import { ChampionRotation } from "@/types/ChampionRotation";
@@ -11,11 +11,11 @@ export async function GET() {
       headers: {
         "Content-Type": "application/json",
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+          "Mozilla/5.0 (Windows NT 10.0 ; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
         "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
         "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
         Origin: RIOT_DEVELOPER_ORIGIN_URL,
-        "X-Riot-Token": NEXT_PUBLIC_API_KEY ?? "",
+        "X-Riot-Token": API_KEY ?? "",
       },
     });
     const data: ChampionRotation = await response.json();
--- src/utils/routeHandler/clientApi.ts
+++ src/utils/routeHandler/clientApi.ts
@@ -1,10 +1,11 @@
 "use client";
 
+import { NEXT_PUBLIC_API_KEY } from "@/constants/apiKey";
 import { Champion } from "@/types/Champion";
 
 export const fetchRotationData = async (): Promise<Champion[]> => {
   try {
-    const res = await fetch("/api/rotation/");
+    const res = await fetch(`${NEXT_PUBLIC_API_KEY}/api/rotation/`);
     return await res.json();
   } catch (error) {
     console.error("아이템 데이터를 불러오는 중 오류가 발생했습니다.", error);
